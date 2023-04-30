7aac9a0ac65653ff4e9b4f0946362933992dc146
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-04-30 09:03:01 +0100

    Display ressults

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-
+from itertools import combinations
 
 # Create your views here.
 def index(request):
@@ -8,3 +8,22 @@ def index(request):
             request,
             "core/index.html",
         )
+    if request.method == "POST":
+        start_number = int(request.POST.get("start_number"))
+        stop_number = int(request.POST.get("stop_number"))
+        possible_combinations = list(combinations(range(start_number, stop_number+1),3))
+
+        possible_string_combinations = []
+
+        for combination in possible_combinations:
+            combination_array = []
+            for number in combination:
+                stringed_number =str(number)
+                combination_array.append(stringed_number)
+            possible_string_combinations.append("-".join(combination_array))
+
+        print(possible_string_combinations)
+
+        context = {"combinations": possible_string_combinations}
+
+        return render(request, "core/index.html", context,)
