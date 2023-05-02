a54edcac1a1f96a4837a0c4cae7403adfe0d26b1
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-05-02 11:39:00 +0100

    Change settings

--- combinator/settings.py
+++ combinator/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
-STATIC_ROOT = "static/"
+STATIC_ROOT = "/opt/render/project/src/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
